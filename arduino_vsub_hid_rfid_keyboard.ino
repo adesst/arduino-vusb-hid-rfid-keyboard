457e1bf245f111b0e0a570c7aa159440cc0fec3e
adesanto asman <adesantoasman@gmail.com>
2015-07-16 08:41:48 +0700

    initial commit

---
+++ arduino_vsub_hid_rfid_keyboard.ino
@@ -0,0 +1,298 @@
+
+#include <UsbDevice.h>
+#include <avr/wdt.h>
+
+typedef struct {
+  uint8_t modifier;
+  uint8_t reserved;
+  uint8_t keycode[6];
+} keyboard_report_t;
+
+static keyboard_report_t keyboard_report; // sent to PC
+volatile static uchar LED_state = 0xff; // received from PC
+static uchar idleRate, state, rstate = 0, icounter = 0;
+static uint16_t multiplexer =0, button_release_counter = 0; // repeat rate for keyboards
+static char message[20]; 
+static uchar imsg = 0, iSzMsg = 0;
+
+String inputString = "", prevString="";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+#define SZ_MSG 20
+#define SZ_KEYCODE 6
+#define NUM_LOCK 1
+#define CAPS_LOCK 2
+#define SCROLL_LOCK 4
+
+#define STATE_WAIT 0
+#define STATE_SEND_KEY 1
+#define STATE_RELEASE_KEY 2
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+usbMsgLen_t usbFunctionSetup(uchar data[8]) {
+    usbRequest_t *rq = (usbRequest_t *)data;
+
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {
+        switch(rq->bRequest) {
+        case USBRQ_HID_GET_REPORT: // send "no keys pressed" if asked here
+            // wValue: ReportType (highbyte), ReportID (lowbyte)
+            usbMsgPtr = (unsigned char *)&keyboard_report; // we only have this one
+            keyboard_report.modifier = 0;
+            keyboard_report.keycode[0] = 0;
+            return sizeof(keyboard_report);
+    case USBRQ_HID_SET_REPORT: // if wLength == 1, should be LED state
+            return (rq->wLength.word == 1) ? USB_NO_MSG : 0;
+        case USBRQ_HID_GET_IDLE: // send idle rate to PC as required by spec
+            usbMsgPtr = &idleRate;
+            return 1;
+        case USBRQ_HID_SET_IDLE: // save idle rate as required by spec
+            idleRate = rq->wValue.bytes[1];
+            return 0;
+        }
+    }
+
+    return 0; // by default don't return any data
+}
+
+usbMsgLen_t usbFunctionWrite(uint8_t * data, uchar len) {
+  
+  return 1; // Data read, not expecting more
+}
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+void (*resetFunc)(void) = 0;
+
+void convertUsbKeycode( uchar send_key )
+{
+  if( send_key == '\n')
+  {
+    keyboard_report.modifier = 0;
+    keyboard_report.keycode[0] = 0x28;
+    return;
+  }
+
+  switch( send_key )
+  {
+    case '0':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x27;
+      break;
+
+    case '1':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x1E;
+      break;
+      
+    case '2':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x1F;
+      break;
+
+    case '3':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x20;
+      break;
+      
+    case '4':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x21;
+      break;
+
+    case '5':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x22;
+      break;
+      
+    case '6':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x23;
+      break;
+
+     case '7':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x24;
+      break;
+
+     case '8':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x25;
+      break;
+
+     case '9':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x26;
+      break;
+
+     case '\n':
+      keyboard_report.modifier = 0;
+      keyboard_report.keycode[0] = 0x28;
+      break;
+ 
+    case 'A':
+      keyboard_report.modifier = 2; // shift key
+      keyboard_report.keycode[0] = 0x04;
+      break;
+
+    case 'B':
+      keyboard_report.modifier = 2; // shift key
+      keyboard_report.keycode[0] = 0x05;
+      break;
+      
+    case 'C':
+      keyboard_report.modifier = 2; // shift key
+      keyboard_report.keycode[0] = 0x06;
+      break;
+    
+    case 'D':
+      keyboard_report.modifier = 2; // shift key
+      keyboard_report.keycode[0] = 0x07;
+      break;
+      
+    case 'E':
+      keyboard_report.modifier = 2; // shift key
+      keyboard_report.keycode[0] = 0x08;
+      break;
+      
+    case 'F':
+      keyboard_report.modifier = 2; // shift key
+      keyboard_report.keycode[0] = 0x09;
+      break;
+
+    case 'a':
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0x04;
+      break;
+
+    case 'b':
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0x05;
+      break;
+      
+    case 'c':
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0x06;
+      break;
+    
+    case 'd':
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0x07;
+      break;
+      
+    case 'e':
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0x08;
+      break;
+      
+    case 'f':
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0x09;
+      break;
+
+    default:
+      keyboard_report.modifier = 0; // shift key
+      keyboard_report.keycode[0] = 0; // ?
+  }
+}
+
+// Now only supports letters 'a' to 'z' and 0 (NULL) to clear buttons
+void buildReport(uchar send_key) {
+  
+  if( send_key == NULL )
+  {
+    keyboard_report.modifier = 0;
+    keyboard_report.keycode[0] = 0;
+  }
+  else
+  {
+    convertUsbKeycode(send_key);
+  }
+}
+
+void send_message()
+{
+  if(imsg == iSzMsg)
+  {
+    // reset message
+    for(uchar i=0; i < SZ_MSG; i++)
+      message[i] = NULL;
+
+    message[0] = '0';
+    message[1] = '\n';
+    
+    imsg = 0;
+    
+    // SET state
+    state = STATE_RELEASE_KEY;
+  }
+  else
+  {
+    buildReport(message[ imsg ] );
+    imsg++;
+  }
+}
+
+void setup() {
+  UsbDevice.begin();
+  state = STATE_SEND_KEY;
+
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 20 bytes for the inputString:
+  inputString.reserve(20);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  wdt_reset();
+  usbPoll();
+      
+  if( usbInterruptIsReady() && stringComplete )
+  {
+    if( state != STATE_WAIT )
+    {
+      switch(state) {
+        case STATE_SEND_KEY:
+          send_message();
+          //state = STATE_RELEASE_KEY; // release next <-- taken cared in send_message()
+          break;
+        case STATE_RELEASE_KEY:
+          buildReport(NULL);
+          state = STATE_WAIT; // should not happen
+          resetFunc();
+          break;
+        default:
+          state = STATE_WAIT; // should not happen
+      }
+     
+      usbSetInterrupt((unsigned char*)&keyboard_report, sizeof(keyboard_report));   
+    }
+  }
+}
+
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    if( stringComplete != true)
+    {
+      inputString += inChar;
+      // if the incoming character is a newline, set a flag
+      // so the main loop can do something about it:
+      iSzMsg++;
+      if (inChar == '\n') {
+        stringComplete = true;
+  
+        for(icounter =0; icounter < iSzMsg; icounter++)
+          message[icounter] = inputString[icounter];
+      }
+    }      
+  }
+}
